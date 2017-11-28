6344f1de78fa36e80bd033c951b3136154d0c6ab
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-11-28 18:01:32 -0500

    added country drop down and installed new package via npm

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LandingPage from "./landing-page";
 
 class App extends React.Component {
     render() {
       return (
         <div>
           Hello
+          <LandingPage />
         </div>
       )
     }
