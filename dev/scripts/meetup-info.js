7aec2466d88cad53d577656c36aed35673712b49
Eileen choi <eileen.h.choi@gmail.com>
2017-11-30 14:57:05 -0500

    updated classnames for styling

--- dev/scripts/meetup-info.js
+++ dev/scripts/meetup-info.js
@@ -6,20 +6,23 @@ export default class MeetupInfo extends React.Component {
   }
   render() {
     return (
-        <ul>
-          <li><h2>{this.props.data.name}</h2></li>
-          <li><p>Venue: {this.props.data.venue.name}</p></li>
-          <li><p>Address: {this.props.data.venue.address_1}</p></li>
-          <li>
-            <p>Event URL: <a href={this.props.data.event_url}>{this.props.data.event_url}</a></p>
-          </li>
-          <li>
-            <p>Meetup Time: {getTime(this.props.data.time)}</p>
-          </li>
-          <li>
-            <p>Meetup Date: {getDate(this.props.data.time)}</p>
-          </li>
-        </ul>
+      <div className="outerDiv">
+          <ul className="meetupContent">
+            <li><h2>{this.props.data.name}</h2></li>
+            <li><p>Venue: {this.props.data.venue.name}</p></li>
+            <li><p>Address: {this.props.data.venue.address_1}</p></li>
+            <li>
+              <p>Event URL: <a href={this.props.data.event_url}>{this.props.data.event_url}</a></p>
+            </li>
+            <li>
+              <p>Meetup Time: {getTime(this.props.data.time)}</p>
+            </li>
+            <li>
+              <p>Meetup Date: {getDate(this.props.data.time)}</p>
+            </li>
+          </ul>
+        </div>
+    
     )
   }
 }
