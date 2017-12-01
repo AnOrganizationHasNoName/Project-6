21749aa7bcfb13d495f28628b0ec81f1dae5a507
Barnabas Chun <chun.barnabas@gmail.com>
2017-12-01 14:36:02 -0500

    Added script to return relevant information to user

--- dev/scripts/restaurants.js
+++ dev/scripts/restaurants.js
@@ -7,13 +7,16 @@ class Restaurants extends React.Component {
     render() {
         return (
             <ul className="restaurants">
-                {/* {this.props.data.map(restaurant => {
+                {this.props.data.map(restaurant => {
                     return <li key={restaurant.id}>
                         <h2>{restaurant.name}</h2>
-                        <p>{restaurant.vicinity}</p>
-                        <p>{restaurant.rating}</p>
+                        <p>Address: {restaurant.vicinity}</p>
+                        <p>Phone Number: {restaurant.international_phone_number}</p>
+                        <p>Google Rating: {restaurant.rating}/5</p>
+                        <p>Website: <a href={restaurant.website}>{restaurant.website}</a></p>
+                        <p>Directions: <a href={restaurant.url}>{restaurant.url}</a></p>
                     </li>
-                })} */}
+                })}
             </ul>
         )
     }
