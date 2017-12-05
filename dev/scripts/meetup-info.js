f055dbec3f84ea85cb772242d9d25d492bb472cc
Eileen choi <eileen.h.choi@gmail.com>
2017-12-05 12:14:26 -0500

    meetup styling

--- dev/scripts/meetup-info.js
+++ dev/scripts/meetup-info.js
@@ -7,8 +7,8 @@ export default class Meetups extends React.Component {
   }
   render() {
     return (
-      <div>
-        <button><Link to="/">Return To Home</Link></button>
+      <div className= "wrapper">
+        <button className="homebutton"><Link to="/">Return To Home</Link></button>
         <ul className="meetups clearfix">
           {this.props.data.map(meetup => {
             return <li className="meetup clearfix" key={meetup.id}>
