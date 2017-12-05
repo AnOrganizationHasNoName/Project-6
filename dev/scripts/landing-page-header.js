acebce25023346588dcdf0aae8aeb9ba0c84f34e
Barnabas Chun <chun.barnabas@gmail.com>
2017-12-05 16:59:18 -0500

    Added styles for loading spinner

--- dev/scripts/landing-page-header.js
+++ dev/scripts/landing-page-header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const TitleOnLandingPage = () => {
-    return (
+const TitleOnLandingPage = () => (
         <header className="landingHeader">
             <h1>Travel Meetups</h1>
             <h2 className="app-description">Find upcoming meetups and nearby restaurants in your area!</h2>
         </header>
-    )
-}
+);
 
 export default TitleOnLandingPage;
--- dev/scripts/loading-spinner.js
+++ dev/scripts/loading-spinner.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const LoadingSpinner = () => (
-    <div>
-        <i className="fa fa-spinner fa-spin" /> Loading...
-      </div>
+    <div className="showbox">
+        <div className="loader">
+            <svg className="circular" viewBox="25 25 50 50">
+                <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10" />
+            </svg>
+        </div>
+    </div>
 );
 
 export default LoadingSpinner;
