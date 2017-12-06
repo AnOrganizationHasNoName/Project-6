a49da1187ba39e3bd32a72e7a9df74600e10a06f
Eileen choi <eileen.h.choi@gmail.com>
2017-12-05 19:16:56 -0500

    latest style fix

--- dev/scripts/landing-page.js
+++ dev/scripts/landing-page.js
@@ -19,7 +19,9 @@ class LandingPage extends React.Component {
     }
     handleSubmit(e) {
         e.preventDefault();
+        // call the prop to submit the form with the states filling in the parameters/placeholders
         this.props.formSubmit(this.state.lat, this.state.lon, this.state.categoryInput);
+        // send the user to meetups withRouter
         this.props.history.push('/meetups');
     }
     handleChange(e) {
@@ -54,7 +56,7 @@ class LandingPage extends React.Component {
         // ajax request for meetup categories
         axios({
             method: 'GET',
-            url: 'http://proxy.hackeryou.com',
+            url: 'https://proxy.hackeryou.com',
             dataResponse: 'json',
             paramsSerializer: function (params) {
                 return Qs.stringify(params, { arrayFormat: 'brackets' })
@@ -82,19 +84,19 @@ class LandingPage extends React.Component {
                 <TitleOnLandingPage />
                 <div className="landingInner">
                     <form action="" className="user-form" onSubmit={this.handleSubmit}>
-                      <select name="categoryInput" onChange={this.handleChange} required>
-                        {this.state.meetupCategories.map(category => <option value={category.id} key={category.id}>{category.name}</option>)}
-                      </select>
-                      <input
-                          id="searchTextField"
-                          type="text"
-                          size="50"
-                          placeholder="Enter a city"
-                          name="locationInput"
-                          required
-                      />
-                      <button className="landingButton">Search</button>
-                     </form>
+                        <select name="categoryInput" onChange={this.handleChange} required>
+                            {this.state.meetupCategories.map(category => <option value={category.id} key={category.id}>{category.name}</option>)}
+                        </select>
+                        <input
+                            id="searchTextField"
+                            type="text"
+                            size="50"
+                            placeholder="Enter a city"
+                            name="locationInput"
+                            required
+                        />
+                        <button className="landingButton">Search</button>
+                    </form>
                 </div>
             </div>  
         )
